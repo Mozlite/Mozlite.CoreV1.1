898d7b101198e74d13b52159a28b85979fde79d7
网魂小兵 <myxbing@hotmail.com>
2017-05-12 17:45:22 +0800

    添加样式！

--- Mozlite.Core/Mvc/TagHelpers/CheckboxListTagHelper.cs
+++ Mozlite.Core/Mvc/TagHelpers/CheckboxListTagHelper.cs
@@ -1,7 +1,114 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+/*
+    相关样式：
+    
+.checkboxlist {
+}
 
+    .checkboxlist .checkbox {
+        cursor: pointer;
+        margin-top: 0;
+        margin-bottom: 10px;
+    }
+
+        .checkboxlist .checkbox .box-wrapper {
+            float: left;
+            position: relative;
+            display: inline-block;
+            border: 1px solid #d2d2d2;
+            padding: 0;
+            padding-right: 16px;
+            margin-bottom: 0;
+            width: 16px;
+            height: 15px;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            -webkit-background-clip: padding-box;
+            -moz-background-clip: padding;
+            background-clip: padding-box;
+            -webkit-border-radius: 3px;
+            -moz-border-radius: 3px;
+            border-radius: 3px;
+            -moz-transition: all 300ms ease-in-out;
+            -webkit-transition: all 300ms ease-in-out;
+            -o-transition: all 300ms ease-in-out;
+            transition: all 300ms ease-in-out;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        .checkboxlist .checkbox input {
+            position: absolute;
+            display: block;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            margin: 0;
+            visibility: hidden;
+        }
+
+        .checkboxlist .checkbox span {
+            margin-left: 10px;
+        }
+
+        .checkboxlist .checkbox .box-wrapper .box-checked {
+            position: absolute;
+            display: block;
+            left: 2px;
+            right: 2px;
+            bottom: 2px;
+            top: 2px;
+            -webkit-background-clip: padding-box;
+            -moz-background-clip: padding;
+            background-clip: padding-box;
+            -webkit-border-radius: 2px;
+            -moz-border-radius: 2px;
+            border-radius: 2px;
+            zoom: 1;
+            -webkit-opacity: 0;
+            -moz-opacity: 0;
+            -ms-filter: progid:DXImageTransform.Microsoft.Alpha(Opacity=0);
+            opacity: 0;
+            filter: alpha(opacity=0);
+            -moz-transition: 250ms all cubic-bezier(0.455, 0.030, 0.515, 0.955);
+            -webkit-transition: 250ms all cubic-bezier(0.455, 0.030, 0.515, 0.955);
+            -o-transition: 250ms all cubic-bezier(0.455, 0.030, 0.515, 0.955);
+            transition: 250ms all cubic-bezier(0.455, 0.030, 0.515, 0.955);
+        }
+
+        .checkboxlist .checkbox:hover .box-wrapper,
+        .checkboxlist .radio:hover .box-wrapper {
+            -moz-box-shadow: 0 0 0 2px rgba(0,0,0,.03);
+            -webkit-box-shadow: 0 0 0 2px rgba(0,0,0,.03);
+            box-shadow: 0 0 0 2px rgba(0,0,0,.03);
+        }
+
+        .checkboxlist .checkbox.checked .box-wrapper .box-checked {
+            opacity: 1;
+        }
+
+            .checkboxlist .checkbox.checked .box-wrapper .box-checked:before {
+                font: normal normal normal 14px/1 FontAwesome;
+                font-size: 12px;
+                position: absolute;
+            }
+
+        .checkboxlist .checkbox.checked.checked-style-default .box-wrapper .box-checked {
+            background: #d2d2d2;
+        }
+
+        .checkboxlist .checkbox.checked.checked-style-check .box-wrapper .box-checked:before {
+            content: '\f00c';
+        }
+*/
 namespace Mozlite.Mvc.TagHelpers
 {
     /// <summary>
